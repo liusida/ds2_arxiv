58aee5e3def0fa44151fd91d66c7c5264be6a4f7
sida <sliu1@uvm.edu>
2021-04-08 14:48:36 -0400

    verify=False

--- ds2_arxiv/2.1.get_citations_from_s2.py
+++ ds2_arxiv/2.1.get_citations_from_s2.py
@@ -25,7 +25,7 @@ def get_remote_content_through_a_proxy(url, time_sleep=0.1):
     for retry in range(3):
         try:
             time.sleep(time_sleep)
-            r = myproxy.current_proxy().request('GET', url, timeout=3).data
+            r = myproxy.current_proxy().request('GET', url, timeout=3, verify=False).data
         except Exception as e:
             print(f"HTTPError: {e}")
             myproxy.rotate_proxy(report_bad_proxy=False)    
@@ -111,7 +111,7 @@ def log():
     })
 
 if __name__=="__main__":
-    wandb.init(project="get_citation")
+    wandb.init(project="get_citation_s2")
     
     g_count = len(glob.glob("data/citations_s2/*.json"))
     g_error = 0
