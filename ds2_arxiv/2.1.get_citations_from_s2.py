f0acfcf3f79b0b6b59eaf6e389ba5a387297d519
sida <sliu1@uvm.edu>
2021-04-08 15:26:10 -0400

    save

--- ds2_arxiv/2.1.get_citations_from_s2.py
+++ ds2_arxiv/2.1.get_citations_from_s2.py
@@ -26,7 +26,7 @@ def get_remote_content_through_a_proxy(url, time_sleep=0.1):
         try:
             time.sleep(time_sleep)
             print(f"requesting {url}")
-            myproxy = MyProxy(proxy_disabled=local_debug)
+            myproxy = MyProxy()
             r = myproxy.current_proxy().request('GET', url, timeout=3).data
         except Exception as e:
             print(f"HTTPError: {e}")
