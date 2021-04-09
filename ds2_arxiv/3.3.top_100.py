27a20302ca1684e21507b0c6aa0e79941790589d
sida <sliu1@uvm.edu>
2021-04-09 11:22:38 -0400

    save

--- ds2_arxiv/3.3.top_100.py
+++ ds2_arxiv/3.3.top_100.py
@@ -5,10 +5,12 @@ with open("shared/top_100.pickle", "rb") as f:
     data = pickle.load(f)
 
 idx = np.argsort(data["cites"])[::-1]
-for i in range(100):
-    j = idx[i]
-    url = f"https://arxiv.org/abs/{data['arxiv_ids'][j]}"
-    title = data['titles'][j]
-    title = re.sub(r'\s+', r' ', title)
-
-    print(f"{url} \t {title}")
+with open("shared/top_100.txt", "w") as f:
+    for i in range(100):
+        j = idx[i]
+        url = f"https://arxiv.org/abs/{data['arxiv_ids'][j]}"
+        title = data['titles'][j]
+        title = re.sub(r'\s+', r' ', title)
+        year = data['years'][j]
+        cite = data['cites'][j]
+        print(f"{url} \t {title} ({year})[{cite}]", file=f) 
