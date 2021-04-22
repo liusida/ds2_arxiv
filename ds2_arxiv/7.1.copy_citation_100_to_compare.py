dcf091fe186e1d2cbd1b98b0c1a44cf14556ffeb
Sida <learner.sida.liu@gmail.com>
2021-04-22 19:23:55 -0400

    modified on server

--- ds2_arxiv/7.1.copy_citation_100_to_compare.py
+++ ds2_arxiv/7.1.copy_citation_100_to_compare.py
@@ -1,8 +1,6 @@
 import glob, os, shutil
 import xmltodict
 
-# citations_gscholar = glob.glob("data/citations_gscholar/*")
-# citations_s2 = glob.glob("data/citations_s2/*")
 harvest_LG_AI = glob.glob("data/harvest_LG_AI_100/*")
 
 i=0
@@ -23,14 +21,6 @@ for filename in harvest_LG_AI:
     g = glob.glob(f"data/citations_gscholar/{arxiv_id}:*.txt")
     shutil.copy(g[0], f"data/citation_compare/g/{arxiv_id}.txt")
 
-
-    # citations_gscholar = glob.glob(f"data/citation_compare/g/{arxiv_id}*.txt")
-    # citations_s2 = os.path(f"data/citation_compare/s2/{arxiv_id}.json")
-    # if len(citations_gscholar)!=1 or len(citations_s2)!=1:
-    #     print(arxiv_id)
-    #     print(citations_gscholar)
-    #     print(citations_s2)
     if i%10==0:
         print(f"{i} {arxiv_id}")
     i+=1
-    # break
