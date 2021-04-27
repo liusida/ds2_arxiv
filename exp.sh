f86d9f009d723621b9d1548fce8eea092366b4c8
sida <sliu1@uvm.edu>
2021-04-27 13:30:49 -0400

    save

--- exp.sh
+++ exp.sh
@@ -2,6 +2,14 @@
 set -x
 if true
 then
+    for seed in 70 71 72 73 74 75 76 77 78 79
+    do
+        sbatch -J gpu93  ~/bin/deepgreen.sh ds2 python ds2_arxiv/9.3.new_algo_on_gpu.py --tag=gpu93 --seed=$seed --num_epochs=1e4
+    done
+fi
+
+if false
+then
     for seed in 60 61 62 63 64 65 66 67 68 69
     do
         sbatch -J e7  ~/bin/bluemoon.sh ds2 python ds2_arxiv/9.2.new_algorithm_scheduling.py --tag=e7 --seed=$seed --epoch_steps=1e4 --num_epochs=1e3
