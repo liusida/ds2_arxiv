7c36712136fec4bada82826b5c78c39290a2a2be
sida <sliu1@uvm.edu>
2021-04-26 23:31:51 -0400

    save

--- exp.sh
+++ exp.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -x
 if true
-for seed in 0 1 2 3 4 5 6 7 8 9
-do
-    sbatch -J e7  ~/bin/bluemoon.sh ds2 python ds2_arxiv/9.2.new_algorithm_scheduling.py --tag=e7 --seed=$seed --epoch_steps=1e4 --num_epochs=1e3
-done
+then
+    for seed in 0 1 2 3 4 5 6 7 8 9
+    do
+        sbatch -J e7  ~/bin/bluemoon.sh ds2 python ds2_arxiv/9.2.new_algorithm_scheduling.py --tag=e7 --seed=$seed --epoch_steps=1e4 --num_epochs=1e3
+    done
+fi
