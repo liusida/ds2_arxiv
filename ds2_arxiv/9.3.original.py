3d16081b43d0c40a256f115dd4c987a2b4223b97
sida <sliu1@uvm.edu>
2021-05-02 10:15:45 -0400

    save

--- ds2_arxiv/9.3.original.py
+++ ds2_arxiv/9.3.original.py
@@ -28,10 +28,11 @@ on host, check for confliction, remove conflicted ones
 swap all of them on device
 """
 
+
 @cuda.jit
 def _detect_gpu(matrix, vec, rng_states):
     grid_id = cuda.grid(1)
-    if grid_id<vec.shape[0]:
+    if grid_id < vec.shape[0]:
         l = matrix.shape[0]
         x = int(xoroshiro128p_uniform_float32(rng_states, grid_id) * l)
         y = int(xoroshiro128p_uniform_float32(rng_states, grid_id) * l)
@@ -42,17 +43,20 @@ def _detect_gpu(matrix, vec, rng_states):
                 if matrix[m, n] > 0 or matrix[m_inv, n] > 0:
                     if m != n and m_inv != n:
                         ret += (abs(m-n)-abs(m_inv-n)) * (matrix[m, n] - matrix[m_inv, n])
-        if ret>0:
-            vec[grid_id,0] = x
-            vec[grid_id,1] = y
-            vec[grid_id,2] = ret
+        if ret > 0:
+            vec[grid_id, 0] = x
+            vec[grid_id, 1] = y
+            vec[grid_id, 2] = ret
+
 
 EPSILON = 1e3
+
+
 @cuda.jit
 def _detect_all_gpu(matrix, index, vec):
     """
     """
-    x,y = cuda.grid(2)
+    x, y = cuda.grid(2)
     l = matrix.shape[0]
     if x < l and y < l:
         ret = 0
@@ -62,71 +66,88 @@ def _detect_all_gpu(matrix, index, vec):
                 if matrix[m, n] > 0 or matrix[m_inv, n] > 0:
                     if m != n and m_inv != n:
                         ret += (abs(m-n)-abs(m_inv-n)) * (matrix[m, n] - matrix[m_inv, n])
-        if ret>EPSILON:
+        if ret > EPSILON:
             i = cuda.atomic.add(index, 0, 1)
-            if i<vec.shape[0]:
-                vec[i,0] = x
-                vec[i,1] = y
-                vec[i,2] = ret
+            if i < vec.shape[0]:
+                vec[i, 0] = x
+                vec[i, 1] = y
+                vec[i, 2] = ret
 
-def detect_and_swap_gpu(matrix, indices, seed, mode='random'):
-    threads_per_block, blocks = 128, 128
+
+def detect_and_swap_gpu(matrix, indices, seed, threads_per_block=128, blocks=128, mode='random'):
+    # threads_per_block, blocks = 128, 128
     # wandb.log({"threads_per_block": threads_per_block, "blocks": blocks})
-    if mode=='random':
+    if mode == 'random':
         rng_states = create_xoroshiro128p_states(threads_per_block * blocks, seed=seed)
         vec = np.zeros([threads_per_block * blocks, 3]).astype(int)
-    elif mode=='all':
+    elif mode == 'all':
         vec = np.zeros([4096, 3]).astype(int)
         index = np.zeros([1]).astype(int)
         d_index = cuda.to_device(index)
-        threadsperblock = (32,32)
-        blockspergrid_x = ( matrix.shape[0] + threadsperblock[0] -1) // threadsperblock[0]
-        blockspergrid_y = ( matrix.shape[1] + threadsperblock[1] -1) // threadsperblock[1]
+        threadsperblock = (32, 32)
+        blockspergrid_x = (matrix.shape[0] + threadsperblock[0] - 1) // threadsperblock[0]
+        blockspergrid_y = (matrix.shape[1] + threadsperblock[1] - 1) // threadsperblock[1]
         blockspergrid = (blockspergrid_x, blockspergrid_y)
 
     d_matrix = cuda.to_device(matrix)
     d_vec = cuda.to_device(vec)
 
-    if mode=='random':
+    if mode == 'random':
         _detect_gpu[blocks, threads_per_block](d_matrix, d_vec, rng_states)
-    elif mode=='all':
+    elif mode == 'all':
         _detect_all_gpu[blockspergrid, threadsperblock](d_matrix, d_index, d_vec)
 
-
     vec = d_vec.copy_to_host()
-    vec = vec[~np.all(vec == 0, axis=1)] # select non-zero rows
-    vec = vec[np.argsort(vec[:, 2])[::-1]] # TODO: greedy?
+    vec = vec[~np.all(vec == 0, axis=1)]  # select non-zero rows
+    vec = vec[np.argsort(vec[:, 2])[::-1]]  # TODO: greedy?
     # print(vec[:5])
     vec_detected = vec.shape[0]
-    print(vec.shape)
+    # print(vec.shape)
     # remove conflicted rows
     visited = {}
     selected = []
     for i in range(vec.shape[0]):
-        if vec[i,0] not in visited and vec[i,1] not in visited:
+        if vec[i, 0] not in visited and vec[i, 1] not in visited:
             selected.append(i)
-            visited[vec[i,0]] = 1
-            visited[vec[i,1]] = 1
+            visited[vec[i, 0]] = 1
+            visited[vec[i, 1]] = 1
     vec = vec[selected, :]
-    print(vec.shape)
+    # print(vec.shape)
     for i in range(vec.shape[0]):
-        swap_inplace(matrix, indices, vec[i,0], vec[i,1])
+        swap_inplace(matrix, indices, vec[i, 0], vec[i, 1])
     vec_swapped = vec.shape[0]
-    wandb.log({"detected": vec_detected, "swapped": vec_swapped})
-    return matrix, indices, vec_detected
+    # wandb.log({"detected": vec_detected, "swapped": vec_swapped})
+    return matrix, indices, vec_detected, vec_swapped
 
-def save_pic(elements, title=""):
-    ret = loss_gpu(elements)
-    record = {
-        "loss": ret,
-    }
-    wandb.log(record)
-    print(f"loss: {ret}")
-    im = np.array(elements / elements.max() * 255, dtype = np.uint8)
-    im_color = cv2.applyColorMap(im, cv2.COLORMAP_HOT)
-    cv2.imwrite(f"tmp/9.3.{title}.png", im_color)
+# def save_pic(elements, title=""):
+#     ret = loss_gpu(elements)
+#     record = {
+#         "loss": ret,
+#     }
+#     wandb.log(record)
+#     print(f"loss: {ret}")
+#     im = np.array(elements / elements.max() * 255, dtype = np.uint8)
+#     im_color = cv2.applyColorMap(im, cv2.COLORMAP_HOT)
+#     cv2.imwrite(f"tmp/9.3.{title}.png", im_color)
 
-if __name__=="__main__":
+
+def save_pic(matrix, indices, title=""):
+    im = np.array(matrix / matrix.max() * 255, dtype=np.uint8)
+    im = 255-im
+    border_width = 2
+    a = np.zeros(shape=[im.shape[0], border_width], dtype=np.uint8)
+    im = np.concatenate([a, im, a], axis=1)
+    b = np.zeros(shape=[border_width, border_width+im.shape[0]+border_width], dtype=np.uint8)
+    im = np.concatenate([b, im, b], axis=0)
+
+    # im_color = cv2.applyColorMap(im, cv2.COLORMAP_HOT)
+    cv2.imwrite(f"tmp/{title}.png", im)
+    wandb.save(f"tmp/{title}.png")
+    np.save(f"tmp/{title}_indicies.npy", indices)
+    wandb.save(f"tmp/{title}_indicies.npy")
+
+
+if __name__ == "__main__":
     def main():
         matrix = np.load("shared/author_similarity_matrix.npy")
         # np.random.seed(1)
@@ -137,27 +158,32 @@ if __name__=="__main__":
         old_matrix = matrix.copy()
         print(loss_gpu(matrix))
         mode = 'random'
+        num_blocks = 256
+        threads_per_block = 128
         for i in range(args.num_epochs):
-            matrix, indices, vec_detected = detect_and_swap_gpu(matrix, indices, seed=i+args.seed, mode=mode)
-            if vec_detected<1 and mode=='all':
-                break # finished
-            elif vec_detected<100: # start enumerate mode
+            matrix, indices, vec_detected, vec_swapped = detect_and_swap_gpu(
+                matrix, indices, threads_per_block=threads_per_block, blocks=num_blocks, seed=i+args.seed, mode=mode)
+            if vec_detected < 1 and mode == 'all':
+                break  # finished
+            elif vec_detected < 100:  # start enumerate mode
+                if num_blocks < 4096:
+                    num_blocks *= 2
+            if vec_detected < 20 and num_blocks == 4096:
                 mode = 'all'
             # else:
             #     mode = 'random'
-            print(i)
-            wandb.log({"epoch": i})
             p1 = loss_gpu(matrix)
-            print(p1)
-            save_pic(matrix, f"step_{i:05}")
+            record = {"step": i, "loss": p1, "detected": vec_detected, "swapped": vec_swapped, "threads_per_block": threads_per_block, "blocks": num_blocks}
+            wandb.log(record)
+            print(record)
+            save_pic(matrix, indices, f"9.3/step_{i:04}")
 
         old_matrix = old_matrix[indices, :]
         old_matrix = old_matrix[:, indices]
         p2 = loss_gpu(old_matrix)
         print(p2)
         print(np.sum(old_matrix), "=", np.sum(matrix))
-        
-        assert(np.isclose(p1, p2)), f"{p1} == {p2}"
 
+        assert(np.isclose(p1, p2)), f"{p1} == {p2}"
 
     main()
