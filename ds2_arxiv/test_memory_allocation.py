2e1205b78bb18844bd61ed18c7fd7f214aa4ed34
Sida <learner.sida.liu@gmail.com>
2021-05-04 10:29:15 -0400

    modified on server

--- ds2_arxiv/test_memory_allocation.py
+++
@@ -1,7 +0,0 @@
-import torch
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-x = torch.rand(size=[128, 393216])
-print(x.shape)
-ret = torch.nn.functional.cosine_similarity(x[:,:,None].to(device), x.t()[None,:,:].to(device))
-print(ret.shape)
