08ca59019c22a1ff9c06eb7f0b066a81859a244f
sida <sliu1@uvm.edu>
2021-05-13 19:16:21 -0400

    save

--- ds2_arxiv/8.1.first_last_author.py
+++ ds2_arxiv/8.1.first_last_author.py
@@ -7,8 +7,8 @@ def abbr(name):
     n = name.split(' ')
     return f"{n[0][0]}. {n[-1]}"
 
-df = pd.DataFrame(columns=['arxiv_id', 's2', 'g'])
-harvest_LG_AI = glob.glob("data/citation_compare/a/*.xml")
+df = pd.DataFrame(columns=['arxiv_id', 's2'])
+harvest_LG_AI = glob.glob("data/arxiv_7636/a/*.xml")
 for filename in harvest_LG_AI:
     with open(filename, "r") as f:
         record_xml = f.read()
@@ -42,17 +42,17 @@ for filename in harvest_LG_AI:
         first_author = last_author = f"{record_dict['authors']['author']['forenames']} {record_dict['authors']['author']['keyname']}"
     # print(arxiv_id, end="\t")
 
-    s2_filename = f"data/citation_compare/s2/{arxiv_id}.json"
+    s2_filename = f"data/arxiv_7636/s2/{arxiv_id}.json"
     with open(s2_filename, "r") as f:
         s2_info = json.load(f)
     s2_citations = len(s2_info['citations'])
     year_s2 = int(s2_info['year'])
     # print("s2: ", s2_citations, end="\t")
 
-    g_filename = f"data/citation_compare/g/{arxiv_id}.txt"
-    with open(g_filename, "r") as f:
-        g_info = f.read()
-    g_citations = int(g_info)
+    # g_filename = f"data/arxiv_7636/g/{arxiv_id}.txt"
+    # with open(g_filename, "r") as f:
+    #     g_info = f.read()
+    # g_citations = int(g_info)
     if (first_author!=s2_info['authors'][0]['name'] and abbr(first_author)!=s2_info['authors'][0]['name']):
         print(f"https://arxiv.org/abs/{arxiv_id} {first_author} v.s. {s2_info['authors'][0]['name']}")
     if (last_author!=s2_info['authors'][-1]['name'] and abbr(last_author)!=s2_info['authors'][-1]['name']):
@@ -66,7 +66,7 @@ for filename in harvest_LG_AI:
         "year_arxiv": year_arxiv,
         "year_s2": year_s2,
         "s2": s2_citations,
-        "g": g_citations,
+        # "g": g_citations,
         "first_author": first_author,
         "last_author": last_author,
         "other_authors": "|".join(other_authors),
@@ -74,4 +74,4 @@ for filename in harvest_LG_AI:
     df = df.append(record, ignore_index=True)
     # break
 print(df)
-df.to_pickle("shared/compare_s2_g_citation.pickle")
+df.to_pickle("shared/7636_citation.pickle")
